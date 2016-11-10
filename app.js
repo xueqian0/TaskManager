78209d582f1cd5d844f4c1dca2c62f6548938d86
薛虔 <1570374376@qq.com>
2016-11-10 21:53:56 +0800

    服务端基本操作

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const bodyparser = require('body-parser'),
+const Task = require('./db.js')
+const app = express()
+app.use(express.static('www'))
+app.use(bodyparser.json())
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000,()=>console.log('监听3000端口成功！'))
